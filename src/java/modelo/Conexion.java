1d017a0254083e5f133609965ea761c63413901e
pabloumg2020 <promeroe1@miumg.edu.gt>
2023-10-18 20:09:58 -0600

    first commit

---
+++ src/java/modelo/Conexion.java
@@ -0,0 +1,36 @@
+
+package modelo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+    public Connection conexionBD;
+    private final String puerto = "3306";
+    private final String bd = "db_empresa";
+    private final String urlConexion = String.format("jdbc:mysql://localhost:3306/%s",bd);
+    //private final String urlConexion = String.format("jdbc:mysql://localhost:%s/%s?serverTimezone=UTC",puerto, bd);
+    private final String usuario = "localhost";
+    private final String contra = "";
+    private final String jdbc = "com.mysql.cj.jdbc.Driver";
+    
+    public void abrir_conexion(){
+        try{
+            Class.forName(jdbc);
+            conexionBD = DriverManager.getConnection(urlConexion,usuario,contra);
+                    
+        }catch(ClassNotFoundException | SQLException ex){
+            System.out.println("Error..." + ex.getMessage());
+        }
+    }
+    public void cerrar_conexion(){
+        try{
+            conexionBD.close();
+                    
+        }catch(SQLException ex){
+            System.out.println("Error..." + ex.getMessage());
+        }
+    }
+    
+}
